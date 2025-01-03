8baa28d5798597c1b826ad7f313df11ae048d352
osamaalanii <zaidosama901@gmail.com>
2025-01-03 22:07:29 +0300

    adding the (char_isalpha,sorting_function,report)function to main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def main():
     path = "books/frankenstein.txt"
     file_content = read_file(path)
-    length =count_words(file_content)
+    wordes_num =count_words(file_content)
     charecters_num = count_characters(file_content)
-    print(charecters_num)
+    alphabet_char = char_isalpha(charecters_num)
+    sorted_char = sorting_function(alphabet_char)
+    charecters_report= report(sorted_char,path,wordes_num)
+
 
 def read_file(path):
     with open(path) as f:
         return f.read()
         
-        
-
 def count_words(content):
     split_words = content.split()
     return len(split_words)
@@ -26,7 +27,26 @@ def count_characters(content):
 
     return charcters_num
 
+def char_isalpha(char_num):
+    X={}
+    for char in char_num:
+        if char.isalpha():
+             X[char] = char_num[char]
+            
 
-    
+    return X
+
+def sorting_function(dictionary):
+    sorted_dic = dict(sorted(dictionary.items(),key= lambda item:item[1],reverse=True))
+    return sorted_dic
+
+def report(char_num,file_path,num_of_words):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{num_of_words} words found in the document")
+    for char in char_num:
+        print(f"The {char} character was found {char_num[char]} times")
+    print("--- End report ---")
+
+
 
 main()
