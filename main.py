2a715d20d60651b9af8d309665ae1833b45bbb3b
osamaalanii <zaidosama901@gmail.com>
2025-01-03 18:03:53 +0300

    update the main.py add(read_file,count_words,count_charecters) functions

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_content = f.read()
-        print(file_content)
+    path = "books/frankenstein.txt"
+    file_content = read_file(path)
+    length =count_words(file_content)
+    charecters_num = count_characters(file_content)
+    print(charecters_num)
 
+def read_file(path):
+    with open(path) as f:
+        return f.read()
+        
+        
+
+def count_words(content):
+    split_words = content.split()
+    return len(split_words)
+
+def count_characters(content):
+    content_lower = content.lower()
+    charcters_num = {}
+    for char in content_lower:
+        charcters_num[char] = 0
+
+    for char in content_lower:
+        charcters_num[char] = charcters_num[char]+1
+
+    return charcters_num
+
+
+    
 
 main()
